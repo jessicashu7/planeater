45dbd1c688e2f8a386db779a498c0d28a1269fb5
jessicashu7 <jessicashu7@gmail.com>
2018-11-25 21:13:17 -0800

    added winter quarter, changed function to work for all quarters

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function total_units_quarter(){
-    var array = document.getElementsByName("num_units");
-    var total = 0;
-    for(var i = 0; i<array.length; i++){
-        total+=Number(array[i].value);
-    }
-    document.getElementById("total_fall_quarter").innerHTML= "Total Units: " + total;
+function total_quarter_units(quarter){
+  var units = quarter.querySelectorAll("input.units");
+  var total = 0;
+  for(var i = 0; i<units.length; i++){
+      total+=Number(units[i].value);
+  }
+  quarter.querySelector("div.total_quarter_units").innerHTML= "Total Units: " + total;
 }

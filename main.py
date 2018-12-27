1be066fa3aa5111e0f52bdb8e1910ab43028652f
jessicashu7 <jessicashu7@gmail.com>
2018-12-26 18:58:27 -0800

    used jinja templating to reduce copy/pasting quarters and years

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def create_app():
 
     @app.route('/')
     def main():
-        return render_template('index.html')
+        #the following lists are used for now to get templating to work
+        #they will be removed later once we get actual database data
+        #the looping in index.html will need to change also
+        y = [1,2,3,4]
+        q = ["Fall", "Winter", "Spring", "Summer"]
+        return render_template('index.html', years=y, quarters=q)
 
     return app
 

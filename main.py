eeaa702acd6b6fc7c09abfe1a2a748c836d31b6d
jessicashu7 <jessicashu7@gmail.com>
2019-01-03 18:48:06 -0800

    changed quarters to be stored as int, added class_num column in database to represent place in order of class within a quarter

--- main.py
+++ main.py
@@ -14,39 +14,42 @@ mysql.init_app(app)
 
 @app.route('/', methods = ['GET'])
 def main():
-    #the following lists are used for now to get templating to work
-    #they will be removed later once we get actual database data
-    #the looping in index.html will need to change also
+    #plan represents all four years. it is a dictionary
+    #which matches year# to dictionary of quarters, which matches
+    #quarter (represented in 0-3) to list of tuples of name, units
+    # 0 is fall, 1 is winter, 2 is spring, 3 is summer
+    # "" and -1 are default values for name, units
     plan = {
         1 : {
-                0 : [("",-1),("",-1),("",-1),("",-1)],
                 1 : [("",-1),("",-1),("",-1),("",-1)],
                 2 : [("",-1),("",-1),("",-1),("",-1)],
-                3 : [("",-1),("",-1),("",-1),("",-1)]
+                3 : [("",-1),("",-1),("",-1),("",-1)],
+                4 : [("",-1),("",-1),("",-1),("",-1)]
             },
         2 : {
-                0 : [("",-1),("",-1),("",-1),("",-1)],
                 1 : [("",-1),("",-1),("",-1),("",-1)],
                 2 : [("",-1),("",-1),("",-1),("",-1)],
-                3 : [("",-1),("",-1),("",-1),("",-1)]
+                3 : [("",-1),("",-1),("",-1),("",-1)],
+                4 : [("",-1),("",-1),("",-1),("",-1)]
             },
         3 : {
-                0 : [("",-1),("",-1),("",-1),("",-1)],
                 1 : [("",-1),("",-1),("",-1),("",-1)],
                 2 : [("",-1),("",-1),("",-1),("",-1)],
-                3 : [("",-1),("",-1),("",-1),("",-1)]
+                3 : [("",-1),("",-1),("",-1),("",-1)],
+                4 : [("",-1),("",-1),("",-1),("",-1)]
             },
         4 : {
-                0 : [("",-1),("",-1),("",-1),("",-1)],
                 1 : [("",-1),("",-1),("",-1),("",-1)],
                 2 : [("",-1),("",-1),("",-1),("",-1)],
-                3 : [("",-1),("",-1),("",-1),("",-1)]
+                3 : [("",-1),("",-1),("",-1),("",-1)],
+                4 : [("",-1),("",-1),("",-1),("",-1)]
             }
 
     }
 
-    #conn = mysql.connect()
-    #cursor = conn.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
 
     return render_template('index.html', plan=plan)
 
@@ -56,18 +59,20 @@ def save():
     conn = mysql.connect()
     cursor = conn.cursor()
     years = [1,2,3,4]
-    quarters = ["Fall", "Winter", "Spring", "Summer"]
+    quarters = [1, 2, 3, 4]
+    classes = [1,2,3,4]
     for y in years:
         for q in quarters:
-            name = request.form[str(y) + "_" + q]
-            units = request.form[str(y) + "_" + q + "_units"]
-            if name != "" or units !="": #if both fields blank, no need to insert in database
-                #format statement puts in name and units (NULL if nothing entered in field)
-                name = (("'{}'".format(name)) if name != "" else "NULL")
-                units = (float(units) if units != "" else "NULL")
-                statement = "INSERT INTO inputclass VALUES (1, '{}', '{}', {}, {}) ON DUPLICATE KEY UPDATE name={}, units={};".format(y,q,name, units, name, units)
-                cursor.execute(statement)
-                conn.commit()
+            for c in classes:
+                name = request.form[str(y) + "_" + str(q) + "_" + str(c)]
+                units = request.form[str(y) + "_" + str(q) + "_" + str(c) + "_units"]
+                if name != "" or units !="": #if both fields blank, no need to insert in database
+                    #format statement puts in name and units (NULL if nothing entered in field)
+                    name = (("'{}'".format(name)) if name != "" else "NULL")
+                    units = (float(units) if units != "" else "NULL")
+                    statement = "INSERT INTO inputclass VALUES (1, {}, {}, {}, {}, {}) ON DUPLICATE KEY UPDATE name={}, units={};".format(y,q, c, name, units, name, units)
+                    cursor.execute(statement)
+                    conn.commit()
     else:
         flash("Your 4 year plan has been successfully saved", 'success')
 

31d399237553570054d58b0c812c5ac3b0ef10be
Brian Chiang <brianchiang2012@gmail.com>
2019-01-24 21:40:29 -0800

    Google API implementation - still needs fixes

--- main.py
+++ main.py
@@ -1,18 +1,90 @@
-from flask import Flask, render_template, url_for, redirect, flash, request
+from flask import Flask, render_template, url_for, redirect, flash, request,g
 from flaskext.mysql import MySQL
+from functools import wraps
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
+
+from flask_oauth2_login import GoogleLogin
+from auth import OAuthSignIn
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'hf9789fdfasd234567jhgdjkjfasd' #won't need this after implmenting google
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #using sqlite for easier testing and implementation, can switch later
-#db = SQLAlchemy(app)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mypassword!'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Lockdown1!'
 app.config['MYSQL_DATABASE_DB'] = 'planeater'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+app.config.update(
+  SECRET_KEY="secret",
+  GOOGLE_LOGIN_REDIRECT_SCHEME="http",
+)
+
+
+# for config in (
+#   "GOOGLE_LOGIN_CLIENT_ID",
+#   "GOOGLE_LOGIN_CLIENT_SECRET",
+# ):
+  # app.config[config] = os.environ[config]
+# google_login = GoogleLogin(app)
+# app.config.from_object('config')
+# app.secret_key = 'this is very secret'
+#
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
+
+
 mysql = MySQL()
 mysql.init_app(app)
+#oauth = OAuth(app)
+
+
+# @login_manager.user_loader
+# def load_user(id):
+#     return User.find_by_id(id)
+#
+#
+# @app.route('/authorize/<provider>')
+# def oauth_authorize(provider):
+#     # Flask-Login function
+#     if not current_user.is_anonymous():
+#         return redirect(url_for('index'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     return oauth.authorize()
+#
+# @app.route('/callback/<provider>')
+# def oauth_callback(provider):
+#     if not current_user.is_anonymous():
+#         return redirect(url_for('index'))
+#     oauth = OAuthSignIn.get_provider(provider)
+#     username, email = oauth.callback()
+#
+#     if email is None:
+#         # I need a valid email address for my user identification
+#         flash('Authentication failed.')
+#         return redirect(url_for('index'))
+#
+#     # Look if the user already exists
+#     user = User.find_or_create_by_email(email)
+#
+#     # Log in the user, by default remembering them for their next visit
+#     # unless they log out.
+#     login_user(user, remember=True)
+#     return redirect(url_for('index'))
+#
+#
+# @app.route('/login', methods = ['POST', 'GET'])
+# def login():
+#     if current_user is not None and current_user.is_authenticated():
+#         user = User()
+#         login_user(user)
+#         return redirect(url_for('main'))
+#     return render_template('login.html',title='Sign In')
+#
+
 
 @app.route('/', methods = ['GET'])
+# @login_required
 def main():
     #plan represents all four years. it is a dictionary
     #which matches year# to dictionary of quarters, which matches
@@ -49,12 +121,12 @@ def main():
 
     conn = mysql.connect()
     cursor = conn.cursor()
-
-
     return render_template('index.html', plan=plan)
 
 @app.route('/save', methods=['POST'])
 def save():
+    #need an if here checking if the person is logged in or not to the webpage
+    #if yes, proceed to database, else prompt them to login first
     #saving data to database here
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -75,9 +147,9 @@ def save():
                     conn.commit()
     else:
         flash("Your 4 year plan has been successfully saved", 'success')
-
     #returning to login page
     return redirect(url_for('main'));
+
 
 
 
--- static/index.js
+++ static/index.js
@@ -31,3 +31,28 @@ function total_units(){
   total = total.toFixed(1);
   document.getElementById("total_units").innerHTML= "Total Units: " + total;
 }
+
+
+
+function onSignIn(googleUser) {
+  // var profile = googleUser.getBasicProfile();
+  // $(".g-signin2").css("display","none");
+  // //$(".data").css("display","block")
+  //
+  // $("#pic").attr('src', profile.getImageUrl())
+  //profile.getEmail()
+
+  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log('Name: ' + profile.getName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+
+// //implemennt sign out next
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//     });
+// }

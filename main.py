5017fb237674510686eaba2d3b8c1d660731a4f1
Brian Chiang <brianchiang2012@gmail.com>
2019-02-02 22:16:26 -0800

    Added a logout button Logout button takes user back to Google signin page Cleared out/commented out mySQL Updated requirements document Made profile picture show up after logging in Added a get_user_data function which returns a dictionary of google information (gmail, name, profile picture, etc) Cleaned up index.js file - Removed signin and signout function from JS file

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from flask import Flask, render_template, url_for, redirect, flash, request,session, jsonify
-from flaskext.mysql import MySQL
 from functools import wraps
 from flask_login import login_user, logout_user, current_user, login_required, LoginManager
+from flask_dance.contrib.google import google
 from flask_oauthlib.client import OAuth
 from auth import OAuthSignIn
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'hf9789fdfasd234567jhgdjkjfasd' #won't need this after implmenting google
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Lockdown1!'
-app.config['MYSQL_DATABASE_DB'] = 'planeater'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-
 app.config['GOOGLE_ID'] = "220365352452-1s6a4kaklt0hop8fnjfti0ojfl9hqgr5.apps.googleusercontent.com"
 app.config['GOOGLE_SECRET'] = "zXmfJl0CljOg-zEew41bLKi2" #currently Brian's test google project
-
 oauth = OAuth(app)
-mysql = MySQL()
-mysql.init_app(app)
 
-google = oauth.remote_app(
+
+
+
+google_auth = oauth.remote_app(
     'google',
     consumer_key=app.config.get('GOOGLE_ID'),
     consumer_secret=app.config.get('GOOGLE_SECRET'),
@@ -37,23 +31,27 @@ google = oauth.remote_app(
 
 @app.route('/login')
 def login():
-    return google.authorize(callback=url_for('authorized', _external=True))
+    return google_auth.authorize(callback=url_for('authorized', _external=True), prompt = "consent")
 
-@app.route('/logout')
+
+@app.route('/logout', methods = ["GET", "POST "])
 def logout():
-    session.pop('google_token', None)
-    return redirect(url_for('index'))
+
+
+    session.clear()
+
+    return redirect(url_for("login"))
+
+
 
 
 @app.route('/', methods = ['GET'])
-# @login_required
 def main():
     #plan represents all four years. it is a dictionary
     #which matches year# to dictionary of quarters, which matches
     #quarter (represented in 0-3) to list of tuples of name, units
     # 0 is fall, 1 is winter, 2 is spring, 3 is summer
     # "" and -1 are default values for name, units
-
     if 'google_token' in session:
         plan = {
             1 : {
@@ -82,10 +80,12 @@ def main():
                 }
 
         }
+        #
+        # conn = mysql.connect()
+        # cursor = conn.cursor()
 
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        return render_template('index.html', plan=plan)
+        user_picture = get_user_data()["picture"] #link to your profile picture
+        return render_template('index.html', plan=plan, picture = user_picture)
     return redirect(url_for('login'))
 
 @app.route('/save', methods=['POST'])
@@ -93,8 +93,8 @@ def save():
     #need an if here checking if the person is logged in or not to the webpage
     #if yes, proceed to database, else prompt them to login first
     #saving data to database here
-    conn = mysql.connect()
-    cursor = conn.cursor()
+    # conn = mysql.connect()
+    # cursor = conn.cursor()
     years = [1,2,3,4]
     quarters = [1, 2, 3, 4]
     classes = [1,2,3,4]
@@ -105,36 +105,36 @@ def save():
                 units = request.form[str(y) + "_" + str(q) + "_" + str(c) + "_units"]
                 if name != "" or units !="": #if both fields blank, no need to insert in database
                     #format statement puts in name and units (NULL if nothing entered in field)
-                    name = (("'{}'".format(name)) if name != "" else "NULL")
-                    units = (float(units) if units != "" else "NULL")
-                    statement = "INSERT INTO inputclass VALUES (1, {}, {}, {}, {}, {}) ON DUPLICATE KEY UPDATE name={}, units={};".format(y,q, c, name, units, name, units)
-                    cursor.execute(statement)
-                    conn.commit()
+                    # name = (("'{}'".format(name)) if name != "" else "NULL")
+                    # units = (float(units) if units != "" else "NULL")
+                    # statement = "INSERT INTO inputclass VALUES (1, {}, {}, {}, {}, {}) ON DUPLICATE KEY UPDATE name={}, units={};".format(y,q, c, name, units, name, units)
+                    # cursor.execute(statement)
+                    # conn.commit()
+                    pass
     else:
         flash("Your 4 year plan has been successfully saved", 'success')
-    #returning to login page
     return redirect(url_for('main'));
 
 
 @app.route('/login/authorized')
 def authorized():
-    resp = google.authorized_response()
+    resp = google_auth.authorized_response()
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
             request.args['error_description']
         )
     session['google_token'] = (resp['access_token'], '')
-    me = google.get('userinfo')
     return redirect(url_for("main"))
     #return jsonify({"data": me.data})
 
-
-@google.tokengetter
+@google_auth.tokengetter
 def get_google_oauth_token():
     return session.get('google_token')
 
-
+def get_user_data():
+    user_data =  google_auth.get('userinfo')
+    return user_data.data #this is a dictionary of user_data
 
 
 if __name__ == "__main__":
